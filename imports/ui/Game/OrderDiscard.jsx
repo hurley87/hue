8fff5b72fb6b3507652c906b1a8afddbf7ad673d
david hurley <dhurls99@gmail.com>
2021-11-05 18:04:28 -0400

    add p tag around text

--- imports/ui/Game/OrderDiscard.jsx
+++ imports/ui/Game/OrderDiscard.jsx
@@ -29,7 +29,7 @@ export const OrderDiscard = ({ game, updateGame, renderCard, userId }) => {
 
     const orderDiscardCurrentUi = (player) => (
         <div>
-            You were ordered to pickup! Discard:
+            <p>You were ordered to pickup! Discard:</p>
             {
                 player.hand.map((card, i) => (<WrappedButton key={i} onClick={() => handleOrderDiscard(card.suit, card.value)}>{renderCard(card.suit, card.value)}</WrappedButton>))
             }
