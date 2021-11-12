4fd91728d6865cc928f15638b4662db7f5b05342
david hurley <dhurls99@gmail.com>
2021-11-12 15:18:48 -0500

    add loserScore to insert score method

--- imports/api/ScoresMethods.js
+++ imports/api/ScoresMethods.js
@@ -1,64 +1,59 @@
-import { check } from 'meteor/check';
-import { ScoresCollection } from '/imports/db/ScoresCollection';
-import { GamesCollection } from '../db/GamesCollection';
- 
+import { check } from "meteor/check";
+import { ScoresCollection } from "/imports/db/ScoresCollection";
+import { GamesCollection } from "../db/GamesCollection";
+
 Meteor.methods({
-  'scores.insert'(score) {
+  "scores.insert"(score) {
     check(score, {
-        winner: String,
-        loser: String,
-        winnerScore: Number,
-        gameId: String,
+      winner: String,
+      loser: String,
+      winnerScore: Number,
+      loserScore: Number,
+      gameId: String,
     });
 
     if (!this.userId) {
-        throw new Meteor.Error('Not authorized.');
+      throw new Meteor.Error("Not authorized.");
     }
 
     try {
-        Meteor.users.update(
-            score.winner,
-            {
-                $set: {
-                    updatedAt: (new Date()).toISOString(),
-                },
-                $inc: {
-                    "profile.score": score.winnerScore,
-                    "profile.wins": score.wins,
-                }
-            }
-        )
-    } catch(e) {
-        console.log("Updating winner error.", e);
+      Meteor.users.update(score.winner, {
+        $set: {
+          updatedAt: new Date().toISOString(),
+        },
+        $inc: {
+          "profile.score": score.winnerScore,
+          "profile.wins": score.wins,
+        },
+      });
+    } catch (e) {
+      console.log("Updating winner error.", e);
     }
 
     try {
-        Meteor.users.update(
-            score.loser,
-            {
-                $set: {
-                    updatedAt: (new Date()).toISOString(),
-                },
-                $inc: {
-                    "profile.losses": 1,
-                }
-            }
-        )
-    } catch(e) {
-        console.log("Updating winner error.", e);
+      Meteor.users.update(score.loser, {
+        $set: {
+          updatedAt: new Date().toISOString(),
+        },
+        $inc: {
+          "profile.losses": 1,
+        },
+      });
+    } catch (e) {
+      console.log("Updating winner error.", e);
     }
 
     try {
-        ScoresCollection.insert({
-            winner: score.winner,
-            loser: score.loser,
-            winnerScore: score.winnerScore,
-            loserScore: score.loserScore,
-            gameId: score.gameId,
-            createdAt: (new Date()).toISOString(),
-        });
-    } catch(e) {
-        console.log("Updating score collection error", e);
+      ScoresCollection.insert({
+        winner: score.winner,
+        loser: score.loser,
+        winnerScore: score.winnerScore,
+        loserScore: score.loserScore,
+        gameId: score.gameId,
+        createdAt: new Date().toISOString(),
+      });
+    } catch (e) {
+      console.log("Updating score collection error", e);
     }
 
     GamesCollection.remove(score.gameId);
