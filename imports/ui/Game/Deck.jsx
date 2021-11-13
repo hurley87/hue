65cd0f902fc77bb75689f828c139d60d5b84b2dd
david hurley <dhurls99@gmail.com>
2021-11-12 19:12:10 -0500

    use smaller cards

--- imports/ui/Game/Deck.jsx
+++ imports/ui/Game/Deck.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Deck = ({ suit, value }) => (
-    <img height="100px" src={`Cards/${suit}${value}.${value === 99 || value === 15 ? 'png' : 'svg'}`}/>
+    <img height="75px" src={`Cards/${suit}${value}.${value === 99 || value === 15 ? 'png' : 'svg'}`}/>
 );
