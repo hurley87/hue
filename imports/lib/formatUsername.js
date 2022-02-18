b11a3af83ef1dd1315ed01a4278376b7ad0c9723
David Hurley <dhurls99@gmail.com>
2022-02-18 16:05:10 -0500

    tr this maybe

--- imports/lib/formatUsername.js
+++ imports/lib/formatUsername.js
@@ -1,7 +1,12 @@
 const formatUsername = (username) => {
-  return username && username.includes(".eth")
-    ? username
-    : username.slice(0, 2) + "..." + username.slice(-4);
+  if (username) {
+    return username.includes(".eth")
+      ? username
+      : username.slice(0, 2) + "..." + username.slice(-4);
+  } else {
+    return;
+    ("...");
+  }
 };
 
 export default formatUsername;
